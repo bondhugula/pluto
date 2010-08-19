b20d56540d889cc09f226fc6e27ecf6ae72ba482
Uday Reddy <udayreddy@gmail.com>
2010-08-18 23:41:25 -0400

    Lose some debug output left unguarded
    Lose debug output printed while converting clan scop to PlutoProg.
    
    Signed-off-by: Uday Reddy <udayreddy@gmail.com>

--- src/program.c
+++ src/program.c
@@ -450,11 +450,11 @@ PlutoProg *scop_to_pluto_prog(scoplib_scop_p scop, PlutoOptions *options)
 		while (!feof(lfp))	{
 			fgets(tmpstr, 256, nlfp);
 			fgets(linearized, 256, lfp);
-			printf("%s\n", tmpstr);
-			printf("%s\n", prog->stmts[i].text);
+			// printf("%s\n", tmpstr);
+			// printf("%s\n", prog->stmts[i].text);
 			if (strstr(tmpstr, prog->stmts[i].text))	{
-				printf("Found substring\n");
-				printf("%s\n", linearized);
+				// printf("Found substring\n");
+				// printf("%s\n", linearized);
 				prog->stmts[i].text = (char *) realloc(prog->stmts[i].text, sizeof(char)*(strlen(linearized)+1));
 				strcpy(prog->stmts[i].text, linearized);
 			}
