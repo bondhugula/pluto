464042e386f1dbbc92771de06c39674e5a78af9e
Prasanth Chatarasi <prasanth@iith.ac.in>
2012-06-26 01:13:11 +0530

    symbol table memory leaks + other
    Signed-off-by: Uday Bondhugula <udayreddy@gmail.com>

--- clan-0.6.0/source/parser.h
+++ clan-0.6.0/source/parser.h
@@ -1,10 +1,9 @@
-
-/* A Bison parser, made by GNU Bison 2.4.1.  */
+/* A Bison parser, made by GNU Bison 2.4.3.  */
 
 /* Skeleton interface for Bison's Yacc-like parsers in C
    
-      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
-   Free Software Foundation, Inc.
+      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006,
+   2009, 2010 Free Software Foundation, Inc.
    
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -168,7 +167,7 @@
 typedef union YYSTYPE
 {
 
-/* Line 1676 of yacc.c  */
+/* Line 1685 of yacc.c  */
 #line 104 "parser.y"
  int value;                     /**< An integer value for integers */
          char * symbol;                 /**< A string for identifiers */
@@ -179,8 +178,8 @@ typedef union YYSTYPE
        
 
 
-/* Line 1676 of yacc.c  */
-#line 184 "parser.h"
+/* Line 1685 of yacc.c  */
+#line 183 "parser.h"
 } YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
 # define yystype YYSTYPE /* obsolescent; will be withdrawn */
