74e1132b0bc4f2dec335364cac1ecda19b3144be
Uday Bondhugula <udayreddy@gmail.com>
2012-07-28 14:51:03 +0530

    LGPLv2 for libpluto
    Signed-off-by: Uday Bondhugula <udayreddy@gmail.com>

--- src/libpluto.c
+++ src/libpluto.c
@@ -1,3 +1,22 @@
+/*
+ * libpluto - a library interface to Pluto
+ * 
+ * Copyright (C) 2012 Uday Kumar Bondhugula
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * A copy of the GNU Lesser General Public Licence can be found in the file
+ * `LICENSE.LGPL2' in the top-level directory of this distribution. 
+ *
+ */
 #include "pluto.h"
 #include "constraints.h"
 #include "candl/candl.h"
