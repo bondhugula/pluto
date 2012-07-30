bondhugula/pluto

commit 1fb739434dde50121d285d0b599d73452362bfd1

allow zero row/col matrices
Signed-off-by: Uday Bondhugula <udayreddy@gmail.com>

src/math_support.c
@@ -35,16 +35,19 @@ PlutoMatrix *pluto_matrix_alloc(int alloc_nrows, int alloc_ncols)
     int i;
     PlutoMatrix *mat;
 
+    assert(alloc_nrows >= 0);
+    assert(alloc_ncols >= 0);
+
     mat = (PlutoMatrix *) malloc(sizeof(PlutoMatrix));
 
-    mat->val = (int **) malloc(alloc_nrows*sizeof(int *));
+    mat->val = (int **) malloc(PLMAX(alloc_nrows,1)*sizeof(int *));
 
     for (i=0; i<alloc_nrows; i++) {
-        mat->val[i] = (int *) malloc(alloc_ncols*sizeof(int));
+        mat->val[i] = (int *) malloc(PLMAX(alloc_ncols,1)*sizeof(int));
     }
 
-    mat->alloc_nrows = alloc_nrows;
-    mat->alloc_ncols = alloc_ncols;
+    mat->alloc_nrows = PLMAX(alloc_nrows,1);
+    mat->alloc_ncols = PLMAX(alloc_ncols,1);
 
     mat->nrows = alloc_nrows;
     mat->ncols = alloc_ncols;

commit 79413cdbd708f1d297b89ce1904f9c04d91697d0

matrix_alloc bug introduced by 1fb739434dde50121d285d0b599d73452362bfd1
Signed-off-by: Uday Bondhugula <udayreddy@gmail.com>

src/math_support.c
@@ -42,12 +42,12 @@ PlutoMatrix *pluto_matrix_alloc(int alloc_nrows, int alloc_ncols)
 
     mat->val = (int **) malloc(PLMAX(alloc_nrows,1)*sizeof(int *));
 
-    for (i=0; i<alloc_nrows; i++) {
-        mat->val[i] = (int *) malloc(PLMAX(alloc_ncols,1)*sizeof(int));
-    }
-
     mat->alloc_nrows = PLMAX(alloc_nrows,1);
     mat->alloc_ncols = PLMAX(alloc_ncols,1);
+
+    for (i=0; i<mat->alloc_nrows; i++) {
+        mat->val[i] = (int *) malloc(mat->alloc_ncols*sizeof(int));
+    }
 
     mat->nrows = alloc_nrows;
     mat->ncols = alloc_ncols;