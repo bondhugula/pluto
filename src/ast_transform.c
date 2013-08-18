225d899b6e51b136b55ff3b960d811ebebeff91d
Uday Bondhugula <udayreddy@gmail.com>
2013-08-18 11:54:43 +0530

    distmem update for ast_transform.c
    Signed-off-by: Uday Bondhugula <udayreddy@gmail.com>

--- src/ast_transform.c
+++ src/ast_transform.c
@@ -29,41 +29,53 @@
 #include "cloog/cloog.h"
 
 /*
- * Clast-based parallel loop marking */
+ * Clast-based parallel/distributed loop marking */
 void pluto_mark_parallel(struct clast_stmt *root, const PlutoProg *prog,
         CloogOptions *cloogOptions)
 {
     int i, j, nloops, nstmts, nploops;
-    struct clast_for **loops;
     int *stmts;
     assert(root != NULL);
 
     // int filter[1] = {1};
 
     Ploop **ploops = pluto_get_dom_parallel_loops(prog, &nploops);
+    /* Loops should be in the increasing order of their depths */
+    qsort(ploops, nploops, sizeof(Ploop *), pluto_loop_compar);
 
     // pluto_print_depsat_vectors(prog->deps, prog->ndeps, prog->num_hyperplanes);
 
     IF_DEBUG(printf("[pluto_mark_parallel] parallel loops\n"););
     IF_DEBUG(pluto_loops_print(ploops, nploops););
 
+    FILE *pidefs = NULL;
+    if (options->distmem) {
+        pidefs = fopen("pi_defs.h", "w");
+    }
+
     // clast_pprint(stdout, root, 0, cloogOptions);
 
     for (i=0; i<nploops; i++) {
         char iter[5];
+        struct clast_for **loops;
         sprintf(iter, "t%d", ploops[i]->depth+1);
         int *stmtids = malloc(ploops[i]->nstmts*sizeof(int));
         for (j=0; j<ploops[i]->nstmts; j++) {
             stmtids[j] = ploops[i]->stmts[j]->id+1;
         }
 
-        // IF_DEBUG(printf("Looking for loop\n"););
-        // IF_DEBUG(pluto_loop_print(ploops[i]););
-        // IF_DEBUG(printf("%s in \n", iter););
+        IF_DEBUG(printf("Looking for loop\n"););
+        IF_DEBUG(pluto_loop_print(ploops[i]););
         // IF_DEBUG(clast_pprint(stdout, root, 0, cloogOptions););
 
         ClastFilter filter = {iter, stmtids, ploops[i]->nstmts, subset};
         clast_filter(root, filter, &loops, &nloops, &stmts, &nstmts);
+
+        if (options->distmem) {
+            /* The parallel loop shouldn't get separated with distmem */
+            if (nloops >= 2)  clast_pprint(stdout, root, 0, cloogOptions);
+            assert(nloops<=1);
+        }
 
         /* There should be at least one */
         if (nloops==0) {
@@ -74,16 +86,188 @@ void pluto_mark_parallel(struct clast_stmt *root, const PlutoProg *prog,
             continue;
         }else{
             for (j=0; j<nloops; j++) {
-                // printf("Marking %s parallel\n", loops[j]->iterator);
-                if (options->parallel) {
+                IF_DEBUG(printf("Marking %s parallel\n", loops[j]->iterator););
+                if (options->commreport) {
+                    loops[j]->time = 1;
+                    loops[j]->time_name = strdup("t_comp");
+                }
+                if (options->distmem) {
+                    loops[j]->parallel = CLAST_PARALLEL_MPI;
+                    if (options->mpiomp) loops[j]->parallel += CLAST_PARALLEL_OMP;
+                }
+                if (options->parallel && !options->distmem) {
                     loops[j]->parallel = CLAST_PARALLEL_OMP;
                 }
                 loops[j]->private_vars = strdup("lbv,ubv");
+
+                if (options->distmem) {
+                    fprintf(pidefs, "#define _LB_REPLACE_ME_DISTLOOG%d ", i);
+                    pprint_expr(cloogOptions, pidefs, loops[j]->LB);
+                    fprintf(pidefs, "\n");
+                    fprintf(pidefs, "#define _UB_REPLACE_ME_DISTLOOG%d ", i);
+                    pprint_expr(cloogOptions, pidefs, loops[j]->UB);
+                    fprintf(pidefs, "\n");
+                }
             }
         }
         free(stmtids);
         free(loops);
         free(stmts);
+
+        if (options->distmem) {
+            /* MPI parallelize the copy_out statements */
+            /* Time the copy_out statements */
+            int count = 0;
+            stmtids = malloc(prog->nstmts*sizeof(int));
+            for (j=0; j<prog->nstmts; j++) {
+                if ((prog->stmts[j]->type == COPY_OUT
+                            || prog->stmts[j]->type == FOIFI_COPY_OUT)
+                        && pluto_stmt_is_member_of(prog->stmts[j]->parent_compute_stmt, 
+                            ploops[i]->stmts, ploops[i]->nstmts)
+                   ) {
+                    stmtids[count++] = prog->stmts[j]->id+1;
+                    assert(prog->stmts[j]->id != -1);
+                    // printf("S%d\n", stmtids[count-1]);
+                }
+            }
+            /* Get all loops at that depth with copy_out statements */
+            ClastFilter filter1 = {iter, stmtids, count, subset};
+            clast_filter(root, filter1, &loops, &nloops, &stmts, &nstmts);
+            for (j=0; j<nloops; j++) {
+                // printf("Timing and marking copy_out %s parallel\n", loops[j]->iterator);
+                // clast_pprint(stdout, loops[j]->body, 0, cloogOptions);
+                if (options->commreport) {
+                    loops[j]->time = 1;
+                    loops[j]->time_name = strdup("t_pack");
+                }
+                loops[j]->parallel = CLAST_PARALLEL_MPI;
+            }
+            free(stmtids);
+            free(loops);
+            free(stmts);
+
+            /* MPI parallelize the sigma statements */
+            /* Time the sigma statements */
+            count = 0;
+            stmtids = malloc(prog->nstmts*sizeof(int));
+            for (j=0; j<prog->nstmts; j++) {
+                if ((prog->stmts[j]->type == SIGMA)
+                        && pluto_stmt_is_member_of(prog->stmts[j]->parent_compute_stmt, 
+                            ploops[i]->stmts, ploops[i]->nstmts)
+                   ) {
+                    stmtids[count++] = prog->stmts[j]->id+1;
+                    assert(prog->stmts[j]->id != -1);
+                    // printf("S%d\n", stmtids[count-1]);
+                }
+            }
+            /* Get all loops at that depth with sigma statements */
+            ClastFilter filter2 = {iter, stmtids, count, subset};
+            clast_filter(root, filter2, &loops, &nloops, &stmts, &nstmts);
+            for (j=0; j<nloops; j++) {
+                // printf("Timing and marking sigma %s parallel\n", loops[j]->iterator);
+                // clast_pprint(stdout, loops[j]->body, 0, cloogOptions);
+                if (options->commreport) {
+                    loops[j]->time = 1;
+                    loops[j]->time_name = strdup("t_comm");
+                }
+                loops[j]->parallel = CLAST_PARALLEL_MPI;
+            }
+            free(stmtids);
+            free(loops);
+            free(stmts);
+
+            /* Time the copy_in statements */
+            count = 0;
+            stmtids = malloc(prog->nstmts*sizeof(int));
+            for (j=0; j<prog->nstmts; j++) {
+                if ((prog->stmts[j]->type == COPY_IN
+                            || prog->stmts[j]->type == FOIFI_COPY_IN)
+                        && pluto_stmt_is_member_of(prog->stmts[j]->parent_compute_stmt, 
+                            ploops[i]->stmts, ploops[i]->nstmts)
+                   ) {
+                    stmtids[count++] = prog->stmts[j]->id+1;
+                    assert(prog->stmts[j]->id != -1);
+                    // printf("S%d\n", stmtids[count-1]);
+                }
+            }
+            /* Get all loops at that depth with copy_in statements */
+            ClastFilter filter3 = {iter, stmtids, count, subset};
+            clast_filter(root, filter3, &loops, &nloops, &stmts, &nstmts);
+            for (j=0; j<nloops; j++) {
+                // printf("Timing copy_in %s\n", loops[j]->iterator);
+                // clast_pprint(stdout, loops[j]->body, 0, cloogOptions);
+                if (options->commreport) {
+                    loops[j]->time = 1;
+                    loops[j]->time_name = strdup("t_unpack");
+                }
+            }
+            free(stmtids);
+            free(loops);
+            free(stmts);
+
+            /* MPI parallelize the write copy_out statements */
+            /* Time the write copy_out statements */
+            count = 0;
+            stmtids = malloc(prog->nstmts*sizeof(int));
+            for (j=0; j<prog->nstmts; j++) {
+                if (prog->stmts[j]->type == LW_COPY_OUT
+                        && pluto_stmt_is_member_of(prog->stmts[j]->parent_compute_stmt, 
+                            ploops[i]->stmts, ploops[i]->nstmts)
+                   ) {
+                    stmtids[count++] = prog->stmts[j]->id+1;
+                    assert(prog->stmts[j]->id != -1);
+                    // printf("S%d\n", stmtids[count-1]);
+                }
+            }
+            /* Get all loops at that depth with write copy_out statements */
+            ClastFilter filter4 = {iter, stmtids, count, subset};
+            clast_filter(root, filter4, &loops, &nloops, &stmts, &nstmts);
+            for (j=0; j<nloops; j++) {
+                // printf("Timing write copy_out %s\n", loops[j]->iterator);
+                // clast_pprint(stdout, loops[j]->body, 0, cloogOptions);
+                if (options->commreport) {
+                    loops[j]->time = 1;
+                    loops[j]->time_name = strdup("t_writeout");
+                }
+                loops[j]->parallel = CLAST_PARALLEL_MPI;
+            }
+            free(stmtids);
+            free(loops);
+            free(stmts);
+
+            /* Time the write copy_in statements */
+            count = 0;
+            stmtids = malloc(prog->nstmts*sizeof(int));
+            for (j=0; j<prog->nstmts; j++) {
+                if (prog->stmts[j]->type == LW_COPY_IN
+                        && pluto_stmt_is_member_of(prog->stmts[j]->parent_compute_stmt, 
+                            ploops[i]->stmts, ploops[i]->nstmts)
+                   ) {
+                    stmtids[count++] = prog->stmts[j]->id+1;
+                    assert(prog->stmts[j]->id != -1);
+                    // printf("S%d\n", stmtids[count-1]);
+                }
+            }
+            /* Get all loops at that depth with write copy_in statements */
+            ClastFilter filter5 = {iter, stmtids, count, subset};
+            clast_filter(root, filter5, &loops, &nloops, &stmts, &nstmts);
+            for (j=0; j<nloops; j++) {
+                // printf("Timing write copy_in %s\n", loops[j]->iterator);
+                // clast_pprint(stdout, loops[j]->body, 0, cloogOptions);
+                if (options->commreport) {
+                    loops[j]->time = 1;
+                    loops[j]->time_name = strdup("t_writeout");
+                }
+            }
+            free(stmtids);
+            free(loops);
+            free(stmts);
+
+        }
+    }
+
+    if (options->distmem) {
+        fclose(pidefs);
     }
 
     pluto_loops_free(ploops, nploops);
