af94de088cca593894e523e5bee4aa0d8e4e6187
Uday Bondhugula <udayreddy@gmail.com>
2013-08-31 21:20:27 +0530

    Revert " not to refine dep poly due to partial satisfaction"
    This reverts commit edec117df2e1928bd560d5e8a029a2310452f547.

--- src/framework.c
+++ src/framework.c
@@ -896,9 +896,7 @@ static int pluto_remove_satisfied_iterations(Dep *dep, PlutoProg *prog, int leve
     //printf("Constraints are empty: %d\n", !retval);
 
     /* All solutions are those that are satisfied */
-
-    /* UB: commenting this out for now */
-    /* pluto_constraints_subtract(dep->depsat_poly,cst); */
+    pluto_constraints_subtract(dep->depsat_poly,cst);
     pluto_constraints_free(cst);
 
     return retval;
