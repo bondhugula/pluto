87ded7f466de2c9f226bf537abb2f7d44c8b87cf
Uday Bondhugula <uday@csa.iisc.ernet.in>
2015-04-23 17:49:34 +0530

    missed prototype
    Signed-off-by: Uday Bondhugula <uday@csa.iisc.ernet.in>

--- src/ast_transform.h
+++ src/ast_transform.h
@@ -2,4 +2,5 @@
 #include "cloog/cloog.h"
 
 void pluto_mark_parallel(struct clast_stmt *root, const PlutoProg *prog, CloogOptions *options);
+void pluto_mark_parallel_dynschedule(struct clast_stmt *root, const PlutoProg *prog, CloogOptions *options);
 void pluto_mark_vector(struct clast_stmt *root, const PlutoProg *prog, CloogOptions *options);
