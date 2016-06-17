72320771296ffcab45c52b6281e8143e259c411c
Uday Bondhugula <uday@csa.iisc.ernet.in>
2016-06-17 12:50:32 +0530

    remove debug option - missed earlier
    Signed-off-by: Uday Bondhugula <uday@csa.iisc.ernet.in>

--- src/test_libpluto.c
+++ src/test_libpluto.c
@@ -196,7 +196,7 @@ int main()
     options = pluto_options_alloc();
     options->tile = 1;
     options->parallel = 1;
-    options->debug = 1;
+    options->debug = 0;
     options->moredebug = 0;
     options->islsolve = 1;
     options->partlbtile = 1;
