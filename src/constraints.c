a01b9e6883b98c851309830195f9744cf3763efa
aravind <aravind.acharya@csa.iisc.ernet.in>
2018-02-20 19:18:55 +0530

    Modularize GLPK solving routines

--- src/constraints.c
+++ src/constraints.c
@@ -1201,9 +1201,26 @@ void set_glpk_constraints_from_pluto_constraints(glp_prob *lp, const PlutoConstr
     free(coeff);
 }
 
-int64 *pluto_constraints_solve_glpk(glp_prob *lp, const PlutoConstraints *cst)
+/* Retrives ilp solution from the input glpk problem. 
+ * Assumes that the optimal solution exists and has been found*/
+int64 *get_ilp_solution_from_glpk_problem(glp_prob *lp)
 {
-    int j;
+    int i, ncols;
+    int64* sol;
+
+    ncols = glp_get_num_cols(lp);
+    sol = (int64*) malloc (sizeof(int64)*ncols);
+    for (i=0; i<glp_get_num_cols(lp); i++) {
+        double x = glp_mip_col_val(lp, i+1);
+        IF_DEBUG(printf("c%d = %lld, ", i, (int64) round(x)););
+        sol[i] = (int64) round(x);
+    }
+    return sol;
+}
+
+/* Set glpk problem parameters. 
+ * Checks feasibility of the LP problem using simplex */
+void set_glpk_problem_params(glp_prob *lp) {
     if (!options->debug && !options->moredebug) {
         glp_term_out(GLP_OFF);
     }
@@ -1220,7 +1237,30 @@ int64 *pluto_constraints_solve_glpk(glp_prob *lp, const PlutoConstraints *cst)
     glp_scale_prob(lp, GLP_SF_AUTO);
     glp_adv_basis(lp, 0);
     glp_simplex(lp, &parm);
+}
 
+void find_optimal_solution_glpk(glp_prob *lp, double tol)
+{
+    glp_iocp iocp;
+    glp_init_iocp(&iocp);
+    /* The default is 1e-5; one may need to reduce it even further
+     * depending on how large a coefficient we might see */
+    iocp.tol_int = tol;
+    IF_DEBUG(printf("Setting GLPK integer tolerance to %e\n", iocp.tol_int));
+
+    iocp.msg_lev = GLP_MSG_OFF;
+    IF_DEBUG(iocp.msg_lev = GLP_MSG_ON;);
+    IF_MORE_DEBUG(iocp.msg_lev = GLP_MSG_ALL;);
+
+    /* Find optimal solution */
+    glp_intopt(lp, &iocp);
+}
+
+int64 *pluto_constraints_solve_glpk(glp_prob *lp)
+{
+    int64 *sol;
+
+    set_glpk_problem_params(lp);
     int lp_status = glp_get_status(lp);
 
     if (lp_status == GLP_INFEAS || lp_status == GLP_UNDEF) {
@@ -1228,18 +1268,8 @@ int64 *pluto_constraints_solve_glpk(glp_prob *lp, const PlutoConstraints *cst)
         return NULL;
     }
 
-    glp_iocp iocp;
-    glp_init_iocp(&iocp);
-    /* The default is 1e-5; one may need to reduce it even further
-     * depending on how large a coefficient we might see */
-    iocp.tol_int = 1e-7;
-    IF_DEBUG(printf("Setting GLPK integer tolerance to %e\n", iocp.tol_int));
 
-    iocp.msg_lev = GLP_MSG_OFF;
-    IF_DEBUG(iocp.msg_lev = GLP_MSG_ON;);
-    IF_MORE_DEBUG(iocp.msg_lev = GLP_MSG_ALL;);
-
-    glp_intopt(lp, &iocp);
+    find_optimal_solution_glpk(lp, 1e-7);
 
     int ilp_status = glp_mip_status(lp);
 
@@ -1250,13 +1280,7 @@ int64 *pluto_constraints_solve_glpk(glp_prob *lp, const PlutoConstraints *cst)
 
     double z = glp_mip_obj_val(lp);
     IF_DEBUG(printf("z = %lf\n", z););
-    int64* sol = malloc(sizeof(int64)*(cst->ncols-1));
-    for (j=0; j<glp_get_num_cols(lp); j++) {
-        double x = glp_mip_col_val(lp, j+1);
-        IF_DEBUG(printf("c%d = %lld, ", j, (int64) round(x)););
-        sol[j] = (int64) round(x);
-    }
-    IF_DEBUG(printf("\n"););
+    sol = get_ilp_solution_from_glpk_problem(lp);
     glp_delete_prob(lp);
     return sol;
 }
@@ -1293,7 +1317,7 @@ int64 *pluto_prog_constraints_lexmin_glpk(const PlutoConstraints *cst,
     IF_DEBUG(glp_write_lp(lp, NULL, "pluto-debug-glpk.lp"););
 
 
-    sol = pluto_constraints_solve_glpk(lp, cst);
+    sol = pluto_constraints_solve_glpk(lp);
 
     return sol;
 }
