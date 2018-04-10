df67237c7e71c7abed960a3313fbb6637d556a4d
CristianRCV <cristianrcv@gmail.com>
2018-04-10 14:20:19 +0200

    Make script fail on error. Add calls to submodules autogen.sh script

--- autogen.sh
+++ autogen.sh
@@ -1,45 +1,33 @@
-#!/usr/bin/env bash
+#!/bin/bash -e
 
-export BASE=`pwd`
+  BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+  export BASE
+  
+  # ------------------------------------------------
+  # (Re)Generate autotools files
+  # ------------------------------------------------
+  
+  echo -e "\n*** Running autotools on pluto ***"
+  autoreconf -vi
+  
+  echo -e "\n*** Running autotools on isl ***"
+  (cd isl; ./autogen.sh)
+  
+  echo -e "\n*** Running autotools on cloog-isl ***"
+  (cd cloog-isl; ./autogen.sh)
+  
+  echo -e "\n*** Running autotools on piplib ***"
+  (cd piplib; ./autogen.sh)
+  
+  echo -e "\n*** Running autotools on polylib ***"
+  (cd polylib; ./autogen.sh)
+  
+  echo -e "\n*** Running autotools on openscop ***"
+  (cd openscop; ./autogen.sh)
+  
+  echo -e "\n*** Running autotools on candl ***"
+  (cd candl; ./autogen.sh)
+  
+  echo -e "\n*** Running autotools on clan ***"
+  (cd clan; ./autogen.sh)
 
-# ------------------------------------------------
-# (Re)Generate autotools files
-# ------------------------------------------------
-
-echo -e "\n*** Running autotools on pluto ***"
-autoreconf -vi
-
-echo -e "\n*** Running autotools on isl ***"
-(cd isl; ./autogen.sh)
-
-echo -e "\n*** Running autotools on cloog-isl ***"
-cd cloog-isl
-./autogen.sh
-cd ..
-
-echo -e "\n*** Running autotools on piplib ***"
-cd piplib
-./autogen.sh
-cd ..
-
-echo -e "\n*** Running autotools on polylib ***"
-cd polylib
-autoreconf -vi
-cd ..
-
-echo -e "\n*** Running autotools on openscop ***"
-cd openscop
-autoreconf -vi
-cd ..
-
-echo -e "\n*** Running autotools on candl ***"
-cd candl
-autoreconf -vi
-cd ..
-
-echo -e "\n*** Running autotools on clan ***"
-cd clan
-./autogen.sh
-cd ..
-
-cd ..
