1caad85ea436d462ad843f2f586d6190863f8bd6
aravind <aravind@iisc.ac.in>
2019-05-24 23:36:57 +0530

    Fix memory leaks in pluto_mark_parallel (introduced by looking for inner parallel loops when outer parallel loop was removed by clast). Move routine to C99/C++11

--- src/ast_transform.c
+++ src/ast_transform.c
@@ -32,31 +32,25 @@
  * Clast-based parallel loop marking */
 void pluto_mark_parallel(struct clast_stmt *root, const PlutoProg *prog,
                          CloogOptions *cloogOptions) {
-  unsigned i, j, nloops, nstmts, nploops, k;
-  unsigned npbands, ninloops;
-  struct clast_for **loops;
-  int *stmts;
-  Band **pbands;
-  int innermost_split_level;
-  Ploop **inloops, *curr_loop;
   assert(root != NULL);
 
   // int filter[1] = {1};
 
-  /* get dominating band instead of a dominating loop */
+  unsigned nploops;
   Ploop **ploops = pluto_get_dom_parallel_loops(prog, &nploops);
-  pbands = (Band **)malloc(nploops * sizeof(Band *));
+  Band **pbands = (Band **)malloc(nploops * sizeof(Band *));
   // pluto_print_depsat_vectors(prog->deps, prog->ndeps, prog->num_hyperplanes);
 
   IF_DEBUG(printf("[pluto_mark_parallel] parallel loops\n"););
   IF_DEBUG(pluto_loops_print(ploops, nploops););
 
-  for (i = 0; i < nploops; i++) {
+  for (unsigned i = 0; i < nploops; i++) {
+    int innermost_split_level;
     pbands[i] =
         pluto_get_parallel_band(ploops[i], prog, &innermost_split_level);
     assert(pbands[i]->width > 0);
   }
-  npbands = nploops;
+  unsigned npbands = nploops;
   // clast_pprint(stdout, root, 0, cloogOptions);
 
   /* iterate over bands and for each loop in the band do the following */
@@ -64,10 +58,10 @@ void pluto_mark_parallel(struct clast_stmt *root, const PlutoProg *prog,
    * loops */
   //
   // Band iterator
-  for (k = 0; k < npbands; k++) {
+  for (unsigned k = 0; k < npbands; k++) {
     /* Iterate over loops in a band */
     Band *band = pbands[k];
-    curr_loop = pbands[k]->loop;
+    Ploop *curr_loop = pbands[k]->loop;
     /* replace this with a do while loop */
     do {
       /* A scalar dimension can not be parallelized */
@@ -78,7 +72,7 @@ void pluto_mark_parallel(struct clast_stmt *root, const PlutoProg *prog,
       sprintf(iter, "t%d", curr_loop->depth + 1);
       int *stmtids = (int *)malloc(curr_loop->nstmts * sizeof(int));
       unsigned max_depth = 0;
-      for (j = 0; j < curr_loop->nstmts; j++) {
+      for (unsigned j = 0; j < curr_loop->nstmts; j++) {
         Stmt *stmt = curr_loop->stmts[j];
         if (stmt->trans->nrows > max_depth)
           max_depth = stmt->trans->nrows;
@@ -89,6 +83,9 @@ void pluto_mark_parallel(struct clast_stmt *root, const PlutoProg *prog,
       IF_DEBUG(pluto_loop_print(curr_loop););
       // IF_DEBUG(clast_pprint(stdout, root, 0, cloogOptions););
 
+      unsigned nloops, nstmts;
+      int *stmts;
+      struct clast_for **loops;
       ClastFilter filter = {iter, stmtids, (int)curr_loop->nstmts, subset};
       clast_filter(root, filter, &loops, (int *)&nloops, &stmts,
                    (int *)&nstmts);
@@ -100,7 +97,9 @@ void pluto_mark_parallel(struct clast_stmt *root, const PlutoProg *prog,
          * 2) it's a scalar dimension but can't be determined from the
          * trans matrix */
 
-        inloops = pluto_get_loops_immediately_inner(curr_loop, prog, &ninloops);
+        unsigned ninloops;
+        Ploop **inloops =
+            pluto_get_loops_immediately_inner(curr_loop, prog, &ninloops);
 
         /* TODO: Fix this. Ideally a cloog clast has to be created for each loop
          * till it succeeds. Hence a worklist based algrithm is necessary. */
@@ -115,7 +114,7 @@ void pluto_mark_parallel(struct clast_stmt *root, const PlutoProg *prog,
         }
         /* continue; */
       } else {
-        for (j = 0; j < nloops; j++) {
+        for (unsigned j = 0; j < nloops; j++) {
           loops[j]->parallel = CLAST_PARALLEL_NOT;
           char *private_vars = (char *)malloc(512);
           strcpy(private_vars, "lbv,ubv");
@@ -132,6 +131,9 @@ void pluto_mark_parallel(struct clast_stmt *root, const PlutoProg *prog,
           free(private_vars);
         }
         /* A loop has been marked parallel. Move to the next band */
+        free(stmtids);
+        free(loops);
+        free(stmts);
         break;
       }
       free(stmtids);
