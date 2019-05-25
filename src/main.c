4c761dc44a13b8c8d1b5418a259a828316109a7b
Uday Bondhugula <uday@polymagelabs.com>
2019-05-25 14:34:55 +0530

    fix warnings
    Signed-off-by: Uday Bondhugula <uday@polymagelabs.com>

--- src/main.c
+++ src/main.c
@@ -192,8 +192,9 @@ int main(int argc, char *argv[]) {
   int nolastwriter = 0;
 
   const struct option pluto_options[] = {
-    {"disable-param-coeffs", no_argument, &options->disable_param_coeffs, 1},
-    {"disable-neg-coeffs", no_argument, &options->disable_neg_coeffs, 1},
+    {"disable-param-coeffs", no_argument, (int *)&options->disable_param_coeffs,
+     1},
+    {"disable-neg-coeffs", no_argument, (int *)&options->disable_neg_coeffs, 1},
     {"fast-lin-ind-check", no_argument, &options->flic, 1},
     {"flic", no_argument, &options->flic, 1},
     {"tile", no_argument, &options->tile, 1},
